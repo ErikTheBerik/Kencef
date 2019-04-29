8e61f69fba1467673010dd1c53c2fb69fb6c3e05
ErikTheBerik <erik@ahlund.me>
2019-04-29 23:46:02 +0200

    Website is up and running. Service workers wont help us. We need to make it possible to view in portrait mode... bootstrap here we come

--- js/home.js
+++ js/home.js
@@ -1,18 +1,18 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-else
-{
-    alert('No service worker');
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('js/sw.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
+// else
+// {
+//     // alert('No service worker');
+// }
 
 var buttonArray = [];
 
